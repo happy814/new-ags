93d8a74b8406b9153d6827405b8087d838e1b1a5
happy814 <rajdey4356@gmail.com>
2024-07-29 11:41:28 +0600

    button

---
+++ dist/app.js
@@ -0,0 +1,27 @@
+// const menu=document.querySelector(".menu");
+// const togglelcon =document.querySelector(".toggle-lcon")
+// togglelcon.addEventListener("click",() => {
+// menu.classList.toggle("hidden");
+// // togglelcon.classList.toggle("fa-solid");
+// // togglelcon.classList.toggle("fa-bars");
+// }
+
+// )
+
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const navLinks = document.querySelector(".nav-links");
+
+toggleBtn.addEventListener("click", () => {  
+  navLinks.classList.toggle("hidden");
+  
+
+
+
+
+
+
+
+});
+
+
