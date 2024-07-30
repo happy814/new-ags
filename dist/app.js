7237543f97e5bcd0a308c8e2f6dbef94be4bda7f
happy814 <rajdey4356@gmail.com>
2024-07-30 11:40:10 +0600

    new ag

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,7 @@
-// const menu=document.querySelector(".menu");
-// const togglelcon =document.querySelector(".toggle-lcon")
-// togglelcon.addEventListener("click",() => {
-// menu.classList.toggle("hidden");
-// // togglelcon.classList.toggle("fa-solid");
-// // togglelcon.classList.toggle("fa-bars");
-// }
+const slide = document.querySelector ('.slide');
 
-// )
-
-
-const toggleBtn = document.querySelector(".toggle-btn");
-const navLinks = document.querySelector(".nav-links");
-
-toggleBtn.addEventListener("click", () => {  
-  navLinks.classList.toggle("hidden");
-  
-
-
-
-
-
-
-
-});
+function Menu() {
+  slide.classList.toggle('hidden');
+}
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./dist/index.html"],
   theme: {
+    
     extend: {
-      
+      colors: {
+        'body':'#F0F6F6',
+        'nav': '#084B83'
+      },
+        
+  
      
 
     },
